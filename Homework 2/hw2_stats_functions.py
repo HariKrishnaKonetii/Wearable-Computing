97501e92eb6ebc2bfd53e85050218ae0136f494a
HariKrishnaKonetii <hk628@nau.edu>
2026-02-19 22:30:29 -0700

    Delete Homework 2/hw2_stats_functions.py

--- Homework 2/hw2_stats_functions.py
+++
@@ -1,8 +0,0 @@
-# INF632 Homework 2 - Stats Functions (from scratch)
-# Name: Hari Krishna Koneti
-
-## Functions (from scratch)
-### Harmonic Mean, Sample Std Dev, Pooled Std Dev, T-test, ANOVA, Repeated-Measures ANOVA
-
-## Data loading helpers
-ActiGraph raw files contain metadata rows. Fitbit minuteSteps export is wide per hour (measure00..measure59).
